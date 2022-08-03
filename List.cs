c48e3ea21ade54fb73de49233a7b14a2d800f62c
Snehakmohanan <108665736+Snehakmohanan@users.noreply.github.com>
2022-08-03 10:36:01 +0530

    Add files via upload

---
+++ List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracttest1
+{
+    abstract class List
+    {
+        public abstract void search(string name);
+        public abstract void printList(string[] list);
+        public List()
+        {
+
+        }
+    }
+}
---
+++ Namelist.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracttest1
+{
+  
+        class Namelist : List
+        {
+
+        //Array Declrtn
+            private string[] list;
+        //constructor
+            public Namelist(string[] list1)
+            {
+                this.list = list1;
+            }
+            public void setList(string[] list)
+            {
+                this.list = list;
+            }
+            public override void printList(string[] list)
+
+            {
+            
+            // print the array list
+
+                  this.list = list;
+               
+            
+                 foreach (string i in list)
+                {
+                    Console.WriteLine(i);
+
+                 }
+               
+               }
+        public override void search(string list)
+        {
+            // search
+            // return true or false
+
+            
+            bool found = false;
+            foreach (string i in this.list)
+            {
+                if (i == list)
+                {
+                    Console.WriteLine("Search name present in the Namelist");
+
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found == false)
+            {
+                Console.WriteLine("Search name not present in the Namelist");
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] list = { "Sneha", "Arya", "Sruthy", "Roshni", "Ajanya", "Nisha" };
+
+            Namelist nl = new Namelist(list);   // new nameList(list);
+            nl.setList(list);
+            nl.printList(list);
+            Console.ReadLine();
+            nl.search("Sajith");
+            Console.ReadLine();
+
+
+
+        }
+
+       
+    }
+    }
+
+
